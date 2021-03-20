923f30bf8f3e710f0fc25231402c45fe4d6226cb
Dominik Bittner <dobi-tyndur@gmx.net>
2021-03-20 13:54:29 +0100

    Remove unnecessary println!s

--- src/uu/install/src/install.rs
+++ src/uu/install/src/install.rs
@@ -424,11 +424,8 @@ fn copy_files_into_dir(files: &[PathBuf], target_dir: &PathBuf, b: &Behavior) ->
         return 1;
     }
 
-    println!("target_dir: {}", target_dir.display());
-
     let mut all_successful = true;
     for sourcepath in files.iter() {
-        println!("sourcepath: {}", sourcepath.display());
         let targetpath = match sourcepath.as_os_str().to_str() {
             Some(name) => {
                 let source = Path::new(name);
@@ -448,8 +445,6 @@ fn copy_files_into_dir(files: &[PathBuf], target_dir: &PathBuf, b: &Behavior) ->
                 continue;
             }
         };
-
-        println!("TargetPath: {}", targetpath.display());
 
         if copy(sourcepath, &targetpath, b).is_err() {
             all_successful = false;
